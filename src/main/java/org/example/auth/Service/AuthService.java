9e81e6b1fcec7465181ffd75c389e9b6aeae13dc
Mihai <falen.angel12@yahoo.com>
2025-04-01 14:16:53 +0300

    update: added some loggers

--- src/main/java/org/example/auth/Service/AuthService.java
+++ src/main/java/org/example/auth/Service/AuthService.java
@@ -1,7 +1,10 @@
 package org.example.auth.Service;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.example.auth.Pojo.AuthPojo;
 import org.example.utils.ResourecesJsonRead;
 
@@ -12,14 +15,21 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class AuthService {
-    RequestSpecBuilder requestSpecBuilder ;
-    public AuthPojo loginUser(String userName, String password, int expiresInMins){
+    RequestSpecBuilder requestSpecBuilder;
+
+    public AuthPojo loginUser(String userName, String password, int expiresInMins) {
         var loginCredentials = ResourecesJsonRead.getLoginCredentials();
-        return given()
-                .contentType(ContentType.JSON).log().all()
+        Response response = given()
+                .contentType(ContentType.JSON)
                 .body(loginCredentials)
+                .filter(new RequestLoggingFilter())  // Logs formatted request
                 .post()
                 .then()
-                .extract().response().body().as(AuthPojo.class);
+                .extract().response();
+
+// Pretty print the response body before deserializing
+        System.out.println("Response Body:\n" + response.prettyPeek());
+
+        return response.body().as(AuthPojo.class);
     }
 }
