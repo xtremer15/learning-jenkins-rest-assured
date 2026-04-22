a7c3b9448d8dc9aad6aac6d7b1b41b6192f3cecf
Mihai Constantin <mihai_constantin94@yahoo.ro>
2026-04-22 23:54:35 +0300

    feat: added new things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,137 +1,83 @@
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.*;
-import config.EnvironmentResolver;
-
 import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        EnvironmentResolver.resolveTo();
-
+//        EnvironmentResolver.resolveTo();
+        WaitContext.execute(WaitType.API, 200);
     }
 }
 
-//
-//class ConfigLoader {
-//    private static volatile JsonObject instance = null;
-//    private static final String FILE_PATH = "src/main/resources/config.json";
-//
-//    private ConfigLoader() {
-//    }
-//
-//    public static JsonObject readFile() throws IOException {
-//        if (instance == null) {
-//            synchronized (ConfigLoader.class) {
-//                if (instance == null) {
-//                    InputStream rawFile = Files.newInputStream(Paths.get(FILE_PATH));
-//                    String file = new BufferedReader(new InputStreamReader(rawFile)).lines().collect(Collectors.joining("\n"));
-//                    instance = JsonParser.parseString(file).getAsJsonObject();
-//                    ;
-//                }
-//            }
-//        }
-//        return instance;
-//    }
-//
-//    public static JsonElement getKey(String key) {
-//        if (instance == null) throw new IllegalStateException("Config not loaded");
-//        return instance.get(key);
-//    }
-//}
-//
-//enum RequestMethod {
-//    POST("POST"),
-//    PUT("PUT"),
-//    PATCH("PATCH"),
-//    GET("GET"),
-//    DELETE("DELETE");
-//
-//    private final String requestMethod;
-//
-//    RequestMethod(String method) {
-//        this.requestMethod = method;
-//    }
-//
-//    public String getRequestMethod() {
-//        return requestMethod;
-//    }
-//}
-//
-//class ApiRequest {
-//    private String url;
-//    private RequestMethod method;
-//
-//    public ApiRequest(Builder builder) {
-//        this.url = builder.url;
-//        this.method = builder.method;
-//    }
-//
-//    static class Builder {
-//        private String url;
-//        private RequestMethod method;
-//        private HashMap<String, String> headers;
-//        private HashMap<String, String> queryParams;
-//        private String requestBOdy;
-//
-//        public Builder setUrl(String url) {
-//            this.url = url;
-//            return this;
-//        }
-//
-//        public Builder setRequestMethod(RequestMethod method) {
-//            this.method = method;
-//            return this;
-//        }
-//
-//        public Builder setHeaders(String headerKey, String headerValue) {
-//            this.headers.put(headerKey, headerValue);
-//            return this;
-//        }
-//
-//        public Builder setQueryParams(String key, String value) {
-//            this.queryParams.put(key, value);
-//            return this;
-//        }
-//
-//        public final ApiRequest build() {
-//            if (this.url.isEmpty() || this.method.getRequestMethod().isEmpty()) {
-//                throw new IllegalStateException("URL and Request method are missing");
-//            }
-//            return new ApiRequest(this);
-//        }
-//
-//        public Builder execute() throws URISyntaxException {
-//            HttpRequest request = HttpRequest.newBuilder()
-//                    .uri(new URI(this.url))
-//                    .version(HttpClient.Version.HTTP_2)
-//                    .build();
-//
-//            String resp = HttpClient.newHttpClient()
-//                    .sendAsync(request, HttpResponse.BodyHandlers.ofString())
-//                    .thenApply(HttpResponse::body)
-//                    .join();
-//            JsonObject obj = JsonParser.parseString(resp).getAsJsonObject();
-//            return this;
-//        }
-//
-//    }
-//
-//}
+class WaitContext {
+
+    public static void execute(WaitType type, int duration) {
+        WaitStrategy strategy = WaitStrategyFactory.createStrategy(type);
+        strategy.waitFor(duration);
+    }
+}
+
+enum WaitType {
+    ELEMENT,
+    PAGE_LOAD,
+    API
+}
+
+@FunctionalInterface
+interface WaitStrategy {
+    void waitFor(int duration);
+}
+
+
+class WaitForElement implements WaitStrategy {
+    @Override
+    public void waitFor(int duration) {
+        System.out.println("Waiting for element..." + duration + "...seconds");
+        try {
+            Thread.sleep(duration * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+class WaitForApiResponse implements WaitStrategy {
+    @Override
+    public void waitFor(int duration) {
+        System.out.println("Waiting for API response for " + duration + " seconds...");
+        try {
+            Thread.sleep(duration * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+// Strategie 3: Wait pentru animații UI
+class WaitForAnimation implements WaitStrategy {
+    @Override
+    public void waitFor(int duration) {
+        System.out.println("Waiting for animation to finish for " + duration + " seconds...");
+        try {
+            Thread.sleep(duration * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+
+class WaitStrategyFactory {
+
+    public static WaitStrategy createStrategy(WaitType type) {
+        switch (type) {
+            case ELEMENT:
+                return new WaitForElement();
+            case PAGE_LOAD:
+                return new WaitForAnimation();
+            case API:
+                return new WaitForApiResponse();
+            default:
+                throw new IllegalArgumentException("Unknown wait type: " + type);
+        }
+    }
+}
---
+++ src/main/java/Misc.java
@@ -0,0 +1,130 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
+
+public class Misc {
+}//
+//class ConfigLoader {
+//    private static volatile JsonObject instance = null;
+//    private static final String FILE_PATH = "src/main/resources/config.json";
+//
+//    private ConfigLoader() {
+//    }
+//
+//    public static JsonObject readFile() throws IOException {
+//        if (instance == null) {
+//            synchronized (ConfigLoader.class) {
+//                if (instance == null) {
+//                    InputStream rawFile = Files.newInputStream(Paths.get(FILE_PATH));
+//                    String file = new BufferedReader(new InputStreamReader(rawFile)).lines().collect(Collectors.joining("\n"));
+//                    instance = JsonParser.parseString(file).getAsJsonObject();
+//                    ;
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+//
+//    public static JsonElement getKey(String key) {
+//        if (instance == null) throw new IllegalStateException("Config not loaded");
+//        return instance.get(key);
+//    }
+//}
+//
+//enum RequestMethod {
+//    POST("POST"),
+//    PUT("PUT"),
+//    PATCH("PATCH"),
+//    GET("GET"),
+//    DELETE("DELETE");
+//
+//    private final String requestMethod;
+//
+//    RequestMethod(String method) {
+//        this.requestMethod = method;
+//    }
+//
+//    public String getRequestMethod() {
+//        return requestMethod;
+//    }
+//}
+//
+//class ApiRequest {
+//    private String url;
+//    private RequestMethod method;
+//
+//    public ApiRequest(Builder builder) {
+//        this.url = builder.url;
+//        this.method = builder.method;
+//    }
+//
+//    static class Builder {
+//        private String url;
+//        private RequestMethod method;
+//        private HashMap<String, String> headers;
+//        private HashMap<String, String> queryParams;
+//        private String requestBOdy;
+//
+//        public Builder setUrl(String url) {
+//            this.url = url;
+//            return this;
+//        }
+//
+//        public Builder setRequestMethod(RequestMethod method) {
+//            this.method = method;
+//            return this;
+//        }
+//
+//        public Builder setHeaders(String headerKey, String headerValue) {
+//            this.headers.put(headerKey, headerValue);
+//            return this;
+//        }
+//
+//        public Builder setQueryParams(String key, String value) {
+//            this.queryParams.put(key, value);
+//            return this;
+//        }
+//
+//        public final ApiRequest build() {
+//            if (this.url.isEmpty() || this.method.getRequestMethod().isEmpty()) {
+//                throw new IllegalStateException("URL and Request method are missing");
+//            }
+//            return new ApiRequest(this);
+//        }
+//
+//        public Builder execute() throws URISyntaxException {
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(new URI(this.url))
+//                    .version(HttpClient.Version.HTTP_2)
+//                    .build();
+//
+//            String resp = HttpClient.newHttpClient()
+//                    .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                    .thenApply(HttpResponse::body)
+//                    .join();
+//            JsonObject obj = JsonParser.parseString(resp).getAsJsonObject();
+//            return this;
+//        }
+//
+//    }
+//
+//}
+
